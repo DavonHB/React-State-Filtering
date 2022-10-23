5ec52809424cfcbf80b8bd8b34dd1cd46d4eaab7
Nosferatu <davonb113@gmail.com>
2022-10-23 11:24:14 -0500

    filtering functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useMemo } from 'react'
 
 function App() {
   const [items, setItems] = useState([])
+  const [query, setQuery] = useState('')
   const inputRef = useRef()
+
+  const filteredItems = useMemo(() => {
+    return items.filter(item => {
+      return item.toLowerCase().includes(query.toLowerCase())
+  })}, [items, query])
 
   function onSubmit(e) {
     e.preventDefault()
@@ -17,15 +23,15 @@ function App() {
 
   return (
    <>
-   Search
-   <input type="search" />
+   Search:
+   <input value={query}  type="search" onChange={e => setQuery(e.target.value)} />
 
    <form onSubmit={onSubmit}>
     New Item: <input ref={inputRef} type="text" />
     <button type='submit'>Add</button>
    </form>
    <h3>Items:</h3>
-   {items.map(item => (
+   {filteredItems.map(item => (
     <div>{item}</div>
    ))}
    </>
