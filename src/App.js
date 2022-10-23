ad6f5e2ebfb1afc60218948123a762ba000eb787
Nosferatu <davonb113@gmail.com>
2022-10-23 11:09:45 -0500

    basic state setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import { useState, useRef } from 'react'
+
 function App() {
+  const [items, setItems] = useState([])
+  const inputRef = useRef()
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    const value = inputRef.current.value
+    if (value === '') return
+    setItems(previous => {
+      return [...previous, value]
+    })
+    inputRef.current.value = ''
+  }
+
   return (
-   <div></div>
+   <>
+   Search
+   <input type="search" />
+
+   <form onSubmit={onSubmit}>
+    New Item: <input ref={inputRef} type="text" />
+    <button type='submit'>Add</button>
+   </form>
+   <h3>Items:</h3>
+   {items.map(item => (
+    <div>{item}</div>
+   ))}
+   </>
   );
 }
 
